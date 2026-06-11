7dc6d6a2e22278d0edadaa8edc92859441491ea1
PisethMao <pisethmao2002@gmail.com>
2026-06-11 09:25:07 +0700

    init project with 4 spring profile

---
+++ src/test/java/co/istad/smartserve/SmartserveApiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.istad.smartserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartserveApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
