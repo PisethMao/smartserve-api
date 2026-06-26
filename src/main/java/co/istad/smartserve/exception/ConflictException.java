2de39f3061f875eb4fa98c8c3d8d16dcad7ca7eb
PisethMao <pisethmao2002@gmail.com>
2026-06-26 15:57:15 +0700

    finish restaurant_table already

--- src/main/java/co/istad/smartserve/exception/ConflictException.java
+++ src/main/java/co/istad/smartserve/exception/ConflictException.java
@@ -6,16 +6,23 @@ import java.util.Map;
 
 @Getter
 public class ConflictException extends RuntimeException {
+
     private final Map<String, String> validationErrors;
 
     public ConflictException(String message) {
         super(message);
-        this.validationErrors = null;
+        this.validationErrors = Map.of("error", message);
+    }
+
+    public ConflictException(String field, String message) {
+        super(message);
+        this.validationErrors = Map.of(field, message);
     }
 
     public ConflictException(String message, Map<String, String> validationErrors) {
         super(message);
-        this.validationErrors = validationErrors;
+        this.validationErrors = validationErrors == null || validationErrors.isEmpty()
+                ? Map.of("error", message)
+                : validationErrors;
     }
-
 }
